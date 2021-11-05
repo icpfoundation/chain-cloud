6b4e4ae1c09cca6d2292e95c53242a8a323d53f9
0x000000000000000 <2283206327@qq.com>
2021-11-05 16:24:27 +0800

    add chain_cloud canister

--- src/chain_cloud/src/lib.rs
+++ src/chain_cloud/src/lib.rs
@@ -1,7 +1,82 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod event;
+mod operation;
+mod types;
+use context::{metadata, util};
+use ic_cdk::export::candid::Nat;
+use ic_cdk::export::candid::{CandidType, Deserialize};
+use ic_cdk::export::Principal;
+use ic_cdk::print;
+use ic_cdk::storage;
+use ic_cdk_macros::*;
+use std::collections::HashMap;
+use types::*;
+static mut count: Option<Nat> = None;
+static mut Event: Vec<metadata::Metadata> = vec![];
+type CanisterEvent = HashMap<Principal, Vec<usize>>;
+type CallerEvent = HashMap<Principal, Vec<usize>>;
+
+/// Add transaction
+#[update(name = "createEvent")]
+async fn create_event(metadata: metadata::Metadata) -> Result<(), String> {
+    event::create_event(metadata).await
+}
+
+#[query(name = "getCanisterEvent")]
+async fn get_canister_event(
+    canister: Principal,
+    offset: Nat,
+    limit: Nat,
+) -> Vec<metadata::Metadata> {
+    event::get_canister_event(canister, offset, limit).await
+}
+
+#[query(name = "getCallerEvent")]
+async fn get_caller_event(caller: Principal, offset: Nat, limit: Nat) -> Vec<metadata::Metadata> {
+    event::get_caller_event(caller, offset, limit).await
+}
+
+#[query(name = "getLastEvent")]
+async fn get_last_event(limit: Nat) -> Vec<metadata::Metadata> {
+    event::get_last_event(limit).await
+}
+
+#[query(name = "getCanisterLastEvent")]
+async fn get_canister_last_event(canister: Principal, limit: Nat) -> Vec<metadata::Metadata> {
+    event::get_canister_last_event(canister, limit).await
+}
+
+#[query(name = "getCanisterList")]
+async fn get_canister_list() -> Vec<Principal> {
+    event::get_canister_list().await
+}
+
+#[update(name = "getCanisterStatus")]
+async fn get_canister_status(
+    canister: Principal,
+    calltype: CallType,
+) -> Result<CanisterStatusFormat, String> {
+    operation::get_canister_status(canister, calltype).await
+}
+
+#[query(name = "getLocalCanisterList")]
+async fn get_local_canister_list() -> Vec<CanisterStatusFormat> {
+    operation::get_local_canister_list().await
+}
+
+#[query(name = "getBalance")]
+async fn account_balance_dfx(account: String) -> ICPTs {
+
+    operation::account_balance_dfx(account).await
+}
+
+/// Before the upgrade task starts, you need to persist the data in memory
+#[pre_upgrade]
+fn pre_upgrade() {
+    event::pre_upgrade();
+}
+
+/// Before the upgrade task ends, you need to reload the persistent data into memory
+#[post_upgrade]
+fn post_update() {
+    event::post_update();
 }
---
+++ src/chain_cloud/src/operation.rs
@@ -0,0 +1,187 @@
+// use context::metadata::{ Metadata,CanisterStatusResponse,CanisterIdRecord,InstallMode};
+use crate::types;
+use crate::util;
+use ic_cdk::api::call::CallResult;
+use ic_cdk::export::candid::Nat;
+use ic_cdk::export::Principal;
+use ic_cdk::storage;
+use ic_cdk::{api, id, print};
+use ic_cdk_macros::*;
+use num_bigint::BigUint;
+use std::collections::HashMap;
+use std::str;
+use types::*;
+type CanisterInfo = HashMap<Principal, CanisterStatusFormat>;
+pub const LEDGET_CANISTERID: &str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
+
+pub async fn get_canister_status(
+    canister: Principal,
+    calltype: CallType,
+) -> Result<CanisterStatusFormat, String> {
+    let res: Result<CanisterStatusFormat, String> = match calltype {
+        CallType::Local => {
+            let canister_table = storage::get::<CanisterInfo>();
+            if canister_table.contains_key(&canister) {
+                let canister_info = canister_table.get(&canister).unwrap();
+                return Ok(canister_info.clone());
+            }
+            Err("canister not exist".to_string())
+        }
+        CallType::Remote => {
+            print(Principal::management_canister().to_string());
+            canister_status(canister).await
+        }
+    };
+    return res;
+}
+
+pub async fn get_local_canister_list() -> Vec<CanisterStatusFormat> {
+    let local_canister = storage::get::<CanisterInfo>();
+    let mut result: Vec<CanisterStatusFormat> = vec![];
+    for v in local_canister.values() {
+        result.push(v.clone());
+    }
+    return result;
+}
+
+pub async fn account_balance_dfx(account: String) -> ICPTs {
+    let ledger = Principal::from_text(LEDGET_CANISTERID).unwrap();
+    let account = AccountBalanceArgs { account: account };
+
+    let res: Result<(ICPTs,), _> = api::call::call(ledger, "account_balance_dfx", (account,)).await;
+    return res.unwrap().0;
+}
+
+async fn canister_status(canister: Principal) -> Result<CanisterStatusFormat, String> {
+    let canister_id = CanisterIdRecord {
+        canister_id: canister.clone(),
+    };
+    let res: Result<(CanisterStatusResponse,), _> = api::call::call(
+        Principal::management_canister(),
+        "canister_status",
+        (canister_id,),
+    )
+    .await;
+    match res {
+        Ok((data,)) => {
+            let info = format!("{:?}", &data);
+            print(info);
+            let module = match &data.module_hash {
+                Some(v) => Some(BigUint::from_bytes_be(&v).to_str_radix(16)),
+                None => None,
+            };
+            let status_foramt = CanisterStatusFormat {
+                status: data.status,
+                settings: data.settings,
+                module_hash: module,
+                controller: data.controller,
+                memory_size: data.memory_size,
+                cycles: data.cycles,
+            };
+            let canisetr_id = canister.clone();
+            let canister_table = storage::get_mut::<CanisterInfo>();
+            canister_table.insert(canisetr_id, status_foramt.clone());
+            return Ok(status_foramt);
+        }
+        Err(v) => {
+            let info = format!("{:?}", v);
+            return Err(info);
+        }
+    }
+}
+
+// async fn create_canister(controller: Principal, mem: Nat, cycle: Nat) -> Result<Principal, String> {
+//     let cycle = util::nat_to_u64(cycle).unwrap();
+//     let in_arg = In {
+//         settings: Some(CanisterSettings {
+//             controller: Some(controller),
+//             compute_allocation: None,
+//             memory_allocation: Some(Nat::from(mem)),
+//             freezing_threshold: None,
+//         }),
+//     };
+
+//     let canister_info: (CreateCanisterResult,) = api::call::call_with_payment(
+//         Principal::management_canister(),
+//         "create_canister",
+//         (in_arg,),
+//         cycle as u64,
+//     )
+//     .await
+//     .expect("create_canister faile");
+//     const HELLO_WASM: &[u8] = std::include_bytes!("../wasm/hello.wasm");
+//     let res = install_canister(&canister_info.0.canister_id, HELLO_WASM.to_vec(), vec![]).await;
+//     match res {
+//         Ok(()) => {
+//             return Ok(canister_info.0.canister_id);
+//         }
+//         Err(v) => {
+//             return Err(v);
+//         }
+//     };
+// }
+
+pub async fn install_canister(
+    canister_id: &Principal,
+    wasm_module: Vec<u8>,
+    args: Vec<u8>,
+) -> Result<(), String> {
+    let install_config = CanisterInstall {
+        mode: types::InstallMode::Install,
+        canister_id: canister_id.clone(),
+        wasm_module: wasm_module.clone(),
+        arg: args,
+    };
+
+    match api::call::call(
+        Principal::management_canister(),
+        "install_code",
+        (install_config,),
+    )
+    .await
+    {
+        Ok(x) => x,
+        Err((code, msg)) => {
+            return Err(format!(
+                "An error happened during the call: {}: {}",
+                code as u8, msg
+            ))
+        }
+    };
+    Ok(())
+}
+
+#[update(name = "updateSettings")]
+async fn update_settings(
+    canister: Principal,
+    controller: Principal,
+    mem: Nat,
+    cycle: Nat,
+) -> Result<(), String> {
+    let cycle = util::nat_to_u64(cycle).unwrap();
+    let args = CanisterSettings {
+        controller: Some(controller),
+        compute_allocation: None,
+        memory_allocation: Some(Nat::from(mem)),
+        freezing_threshold: None,
+    };
+    let in_arg = UpdateCanister {
+        canister_id: canister,
+        settings: args,
+    };
+    match api::call::call_with_payment(
+        Principal::management_canister(),
+        "update_settings",
+        (in_arg,),
+        cycle as u64,
+    )
+    .await
+    {
+        Ok(()) => {
+            return Ok(());
+        }
+        Err((code, msg)) => {
+            return Err(format!("update canister faile: {}: {}", code as u8, msg));
+        }
+    };
+}
