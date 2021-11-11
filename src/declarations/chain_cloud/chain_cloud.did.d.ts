f9c6743ce74597ec1822b5b134c0e3ad71cd92c0
0x000000000000000 <2283206327@qq.com>
2021-11-11 20:21:29 +0800

    canister did js

--- src/declarations/chain_cloud/chain_cloud.did.d.ts
+++ src/declarations/chain_cloud/chain_cloud.did.d.ts
@@ -1,1 +1,86 @@
 import type { Principal } from '@dfinity/principal';
+export type CallType = { 'Local' : null } |
+  { 'Remote' : null };
+export interface CanisterSettings {
+  'controller' : [] | [Principal],
+  'freezing_threshold' : [] | [bigint],
+  'memory_allocation' : [] | [bigint],
+  'compute_allocation' : [] | [bigint],
+}
+export type CanisterStatus = { 'stopped' : null } |
+  { 'stopping' : null } |
+  { 'running' : null };
+export interface CanisterStatusFormat {
+  'controller' : Principal,
+  'status' : CanisterStatus,
+  'memory_size' : bigint,
+  'cycles' : bigint,
+  'settings' : CanisterSettings,
+  'module_hash' : [] | [string],
+}
+export type CanisterType = { 'ui' : null } |
+  { 'server' : null } |
+  { 'wallet' : null };
+export interface CommitCanister {
+  'controller' : Principal,
+  'status' : CanisterStatus,
+  'principle' : Principal,
+  'balance' : bigint,
+  'memory_size' : bigint,
+  'canister_id' : Principal,
+  'create_time' : bigint,
+  'subnet' : string,
+  'module_hash' : string,
+  'canister_type' : CanisterType,
+}
+export type GetCanisterByIdResult = { 'Ok' : CommitCanister } |
+  { 'Err' : string };
+export interface Metadata {
+  'transaction_time' : bigint,
+  'memo' : string,
+  'cycle' : bigint,
+  'method_name' : string,
+  'canister' : Principal,
+  'caller' : Principal,
+  'stable_size' : bigint,
+}
+export type Result = { 'Ok' : null } |
+  { 'Err' : string };
+export type StatusResult = { 'Ok' : CanisterStatusFormat } |
+  { 'Err' : string };
+export type createCanisterResult = { 'Ok' : Principal } |
+  { 'Err' : string };
+export type updateResult = { 'Ok' : null } |
+  { 'Err' : string };
+export interface _SERVICE {
+  'commitCanister' : (arg_0: CommitCanister) => Promise<undefined>,
+  'createEvent' : (arg_0: Metadata) => Promise<Result>,
+  'getCallerEvent' : (
+      arg_0: Principal,
+      arg_1: bigint,
+      arg_2: bigint,
+    ) => Promise<Array<Metadata>>,
+  'getCanisterById' : (arg_0: Principal, arg_1: Principal) => Promise<
+      GetCanisterByIdResult
+    >,
+  'getCanisterByPrinciple' : (arg_0: Principal) => Promise<
+      Array<CommitCanister>
+    >,
+  'getCanisterEvent' : (
+      arg_0: Principal,
+      arg_1: bigint,
+      arg_2: bigint,
+    ) => Promise<Array<Metadata>>,
+  'getCanisterEventByTime' : (arg_0: Principal, arg_1: bigint) => Promise<
+      Array<Metadata>
+    >,
+  'getCanisterLastEvent' : (arg_0: Principal, arg_1: bigint) => Promise<
+      Array<Metadata>
+    >,
+  'getCanisterList' : () => Promise<Array<Principal>>,
+  'getCanisterStatus' : (arg_0: Principal, arg_1: CallType) => Promise<
+      StatusResult
+    >,
+  'getLastEvent' : (arg_0: bigint) => Promise<Array<Metadata>>,
+  'getLocalCanisterList' : () => Promise<Array<CanisterStatusFormat>>,
+}
--- src/declarations/chain_cloud/chain_cloud.did.js
+++ src/declarations/chain_cloud/chain_cloud.did.js
@@ -1,1 +1,104 @@
-
+export const idlFactory = ({ IDL }) => {
+  const CanisterStatus = IDL.Variant({
+    'stopped' : IDL.Null,
+    'stopping' : IDL.Null,
+    'running' : IDL.Null,
+  });
+  const CanisterType = IDL.Variant({
+    'ui' : IDL.Null,
+    'server' : IDL.Null,
+    'wallet' : IDL.Null,
+  });
+  const CommitCanister = IDL.Record({
+    'controller' : IDL.Principal,
+    'status' : CanisterStatus,
+    'principle' : IDL.Principal,
+    'balance' : IDL.Nat,
+    'memory_size' : IDL.Nat,
+    'canister_id' : IDL.Principal,
+    'create_time' : IDL.Nat,
+    'subnet' : IDL.Text,
+    'module_hash' : IDL.Text,
+    'canister_type' : CanisterType,
+  });
+  const Metadata = IDL.Record({
+    'transaction_time' : IDL.Nat,
+    'memo' : IDL.Text,
+    'cycle' : IDL.Nat,
+    'method_name' : IDL.Text,
+    'canister' : IDL.Principal,
+    'caller' : IDL.Principal,
+    'stable_size' : IDL.Nat,
+  });
+  const Result = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text });
+  const GetCanisterByIdResult = IDL.Variant({
+    'Ok' : CommitCanister,
+    'Err' : IDL.Text,
+  });
+  const CallType = IDL.Variant({ 'Local' : IDL.Null, 'Remote' : IDL.Null });
+  const CanisterSettings = IDL.Record({
+    'controller' : IDL.Opt(IDL.Principal),
+    'freezing_threshold' : IDL.Opt(IDL.Nat),
+    'memory_allocation' : IDL.Opt(IDL.Nat),
+    'compute_allocation' : IDL.Opt(IDL.Nat),
+  });
+  const CanisterStatusFormat = IDL.Record({
+    'controller' : IDL.Principal,
+    'status' : CanisterStatus,
+    'memory_size' : IDL.Nat,
+    'cycles' : IDL.Nat,
+    'settings' : CanisterSettings,
+    'module_hash' : IDL.Opt(IDL.Text),
+  });
+  const StatusResult = IDL.Variant({
+    'Ok' : CanisterStatusFormat,
+    'Err' : IDL.Text,
+  });
+  return IDL.Service({
+    'commitCanister' : IDL.Func([CommitCanister], [], []),
+    'createEvent' : IDL.Func([Metadata], [Result], []),
+    'getCallerEvent' : IDL.Func(
+        [IDL.Principal, IDL.Nat, IDL.Nat],
+        [IDL.Vec(Metadata)],
+        ['query'],
+      ),
+    'getCanisterById' : IDL.Func(
+        [IDL.Principal, IDL.Principal],
+        [GetCanisterByIdResult],
+        ['query'],
+      ),
+    'getCanisterByPrinciple' : IDL.Func(
+        [IDL.Principal],
+        [IDL.Vec(CommitCanister)],
+        ['query'],
+      ),
+    'getCanisterEvent' : IDL.Func(
+        [IDL.Principal, IDL.Nat, IDL.Nat],
+        [IDL.Vec(Metadata)],
+        ['query'],
+      ),
+    'getCanisterEventByTime' : IDL.Func(
+        [IDL.Principal, IDL.Nat],
+        [IDL.Vec(Metadata)],
+        ['query'],
+      ),
+    'getCanisterLastEvent' : IDL.Func(
+        [IDL.Principal, IDL.Nat],
+        [IDL.Vec(Metadata)],
+        ['query'],
+      ),
+    'getCanisterList' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'getCanisterStatus' : IDL.Func(
+        [IDL.Principal, CallType],
+        [StatusResult],
+        [],
+      ),
+    'getLastEvent' : IDL.Func([IDL.Nat], [IDL.Vec(Metadata)], ['query']),
+    'getLocalCanisterList' : IDL.Func(
+        [],
+        [IDL.Vec(CanisterStatusFormat)],
+        ['query'],
+      ),
+  });
+};
+export const init = ({ IDL }) => { return []; };
