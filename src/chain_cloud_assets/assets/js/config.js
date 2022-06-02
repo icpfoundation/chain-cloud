fa33dda0d296f466ae24e79d79300a4f7cfc18f7
lyswifter <liuyong@tianrukj.com>
2022-06-02 16:32:40 +0800

    adjust canister ids to main network

--- src/chain_cloud_assets/assets/js/config.js
+++ src/chain_cloud_assets/assets/js/config.js
@@ -1,3 +1,5 @@
+
+// development
 // const MANAGE_CANISTER_LOCALNET = "r7inp-6aaaa-aaaaa-aaabq-cai";
 // const IMAGESTORE_CANISTER_LOCALNET = "ryjl3-tyaaa-aaaaa-aaaba-cai";
 // const CANISTER_LOG_LOCALNET = "rrkah-fqaaa-aaaaa-aaaaq-cai"
@@ -8,9 +10,10 @@
 // const TEST_PROJECT_CANISTER = [TEST_CANISTER];
 // export { MANAGE_CANISTER_LOCALNET, IMAGESTORE_CANISTER_LOCALNET, CANISTER_LOG_LOCALNET, TEST_USER, TEST_GROUP_ID, TEST_CANISTER, TEST_PROJECT_ID, TEST_PROJECT_CANISTER }
 
-const MANAGE_CANISTER_LOCALNET = "rdmx6-jaaaa-aaaaa-aaadq-cai";
-const IMAGESTORE_CANISTER_LOCALNET = "renrk-eyaaa-aaaaa-aaada-cai";
-const CANISTER_LOG_LOCALNET = "rno2w-sqaaa-aaaaa-aaacq-cai"
+// production
+const MANAGE_CANISTER_LOCALNET = "jqhuf-2aaaa-aaaan-qahxq-cai";
+const IMAGESTORE_CANISTER_LOCALNET = "ln22n-jiaaa-aaaan-qahya-cai";
+const CANISTER_LOG_LOCALNET = "ldyxf-syaaa-aaaan-qahza-cai";
 const TEST_USER = "2vxsx-fae";
 const TEST_GROUP_ID = 1;
 const TEST_CANISTER = "qoctq-giaaa-aaaaa-aaaea-cai"
